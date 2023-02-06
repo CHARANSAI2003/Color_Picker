fee8da38cc9ed96ad656b312c73051f00fb7d727
Charan Sai B <105934273+CHARANSAI2003@users.noreply.github.com>
2023-02-06 10:46:26 +0530

    Add files via upload

---
+++ Color Picker.js
@@ -0,0 +1,19 @@
+function changeBgToGreyAndUpdateText() {
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+}
+
+function changeBgToGreenAndUpdateText() {
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97";
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+}
+
+function changeBgToBlueAndUpdateText() {
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+}
+
+function changeBgToPurpleAndUpdateText() {
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+}
